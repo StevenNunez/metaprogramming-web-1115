a90683d7612f8b896ab0762b2a09908779c552d4
Aemrox <adam.may2164@gmail.com>
2015-12-11 13:13:54 -0500

    goteeeeem

--- any_assignment.rb
+++ any_assignment.rb
@@ -1,22 +1,42 @@
 require 'pry'
+require_relative 'class_macro.rb'
+
+module Dynamic
+  def self.create_accessors(*properties)
+    properties.each do |property|
+      define_method property do
+        instance_variable_get("@#{property}")
+      end
+
+      define_method "#{property}=" do |new_value|
+        instance_variable_set("@#{property}", new_value)
+      end
+    end
+  end
+end
+
 class Person
+  extend Dynamic
   attr_accessor :name, :age, :height
   def initialize(**args)
     args.each do |property, value|
+      unless self.methods.include?(property)
+        self.class.create_accessors(property)
+      end
       self.public_send("#{property}=", value)
     end
   end
 
   def method_missing(meth, *args)
-    meth.to_s.end_with?("=")
-  #   if meth == :zorp!
-  #     "Zip Zip Zoop"
-  #   else
-  #     super
-  #   end
-  # end
-end
+    # binding.pry
+    if meth.to_s.end_with?("=")
+      self.class.create_accessors(meth.to_s.gsub("=",""))
+      # binding.pry
+      self.send(meth,args.first)
+    else
+      super(meth)
+    end
+  end
 
-steven = Person.new
-puts steven.zorp!
-puts steven.zlarking!
+
+end
